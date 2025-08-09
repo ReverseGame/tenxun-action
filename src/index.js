8bc2ca69a7d5529f8307453b2a09eef498e07937
taochen <15736755067@163.com>
2025-08-09 13:22:36 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import * as core from '@actions/core'
 export async function run() {
   // 动态导入 tencent_cloud 模块
   const tencent_cloud = await import('@reverse_game/tencent_cloud')
-
+  const core = await import('@actions/core')
   let action = core.getInput('action')
   if (action === 'create_image') {
     return await tencent_cloud.create_image(
