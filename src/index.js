f258faf6ba826b41cfb15280aaa3822d1c6b5e01
taochen <15736755067@163.com>
2025-08-09 12:53:22 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-/**
- * The entrypoint for the action. This file simply imports and runs the action's
- * main logic.
- */
-import { run } from './main.js'
+import * as tencent_cloud from '@reverse_game/tencent_cloud'
+import * as core from '@actions/core'
+export async function run() {
+  let action = core.getInput('action')
+  if (action === 'create_image') {
+    return await tencent_cloud.create_image(
+      core.getInput('secret_id'),
+      core.getInput('secret_key'),
+      core.getInput('region'),
+      core.getInput('instance_name_prefix')
+    )
+  } else if (action === 'get_ips') {
+    return await tencent_cloud.get_ips(
+      core.getInput('secret_id'),
+      core.getInput('secret_key'),
+      core.getInput('region'),
+      core.getInput('instance_name_prefix')
+    )
+  } else if (action === 'create_image_sync_as') {
+    return await tencent_cloud.create_image_sync_as(
+      core.getInput('secret_id'),
+      core.getInput('secret_key'),
+      core.getInput('region'),
+      core.getInput('instance_name_prefix'),
+      core.getInput('launch_configuration_id')
+    )
+  }
+}
 
-/* istanbul ignore next */
 run()
